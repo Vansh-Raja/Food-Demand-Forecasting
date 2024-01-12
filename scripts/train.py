236c0daec1d7c527a1441370072465201b46cb79
UrAvgProGamer <vanshraja32@gmail.com>
2024-01-13 03:01:48 +0530

    adding mlflow to the models

--- scripts/train.py
+++ scripts/train.py
@@ -14,7 +14,9 @@ gb_regressor = GradientBoostingRegressor()
 from sklearn.linear_model import Lasso
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import ExtraTreesRegressor
-from sklearn.ensemble import AdaBoostRegressor    
+from sklearn.ensemble import AdaBoostRegressor
+import mlflow
+import mlflow.sklearn
 
 # Importing the dataset
 print("Importing the dataset.. ")
@@ -89,7 +91,7 @@ def RandomForestRegressor():
         'n_estimators': [int(x) for x in range(100, 1001, 100)],
         'max_features': ['auto', 'sqrt', 'log2'],
         'max_depth': [int(x) for x in range(10, 110, 10)],
-        'min_samples_split': [2, 5, 10],
+        'min_samples_split': [2, 5, 10  ],
         'min_samples_leaf': [1, 2, 4]
     }
 
@@ -113,28 +115,141 @@ def RandomForestRegressor():
     print(f"Mean Squared Error: {mse}")
     
 def XGBoostRegressor():
-    # XGBoost Regressor
-    print("XGBoost Regressor")
     
+    with mlflow.start_run():
+        
+        param_dist2 = {
+        'n_estimators': [int(x) for x in range(100, 1001, 100)],
+        'learning_rate': [0.01, 0.1, 0.2, 0.3],
+        'max_depth': [3, 4, 5, 6, 7, 8, 9, 10],
+        'subsample': [0.8, 0.9, 1.0],
+        'colsample_bytree': [0.8, 0.9, 1.0],
+        }
+        
+        random_search = RandomizedSearchCV(
+            xgb_model,
+            param_distributions=param_dist2,
+            n_iter=10,  # Adjust the number of iterations as needed
+            scoring='neg_mean_squared_error',
+            cv=5,  # Number of cross-validation folds
+            verbose=1,
+            n_jobs=-1,  # Use all available CPU cores
+            random_state=42
+        )
+        random_search.fit(X_train, Y_train)
+        
+        best_params = random_search.best_params_
+        best_model = random_search.best_estimator_
+        
+        # Logging best params
+        for param_name, param_value in best_params.items():
+            mlflow.log_param(param_name, param_value)
+        
+        y_pred = best_model.predict(X_test)
+        mse = mean_squared_error(Y_test, y_pred)
+        
+        # Logging best score
+        mlflow.log_metric("mse", mse)
+        print(f"Mean Squared Error: {mse}")
+        
+        # Logging the model to mlflow
+        mlflow.sklearn.log_model(best_model, "XGBRegressor")
+     
 def GradientBoostingRegressor():
     # Gradient Boosting Regressor
     print("Gradient Boosting Regressor")
     
 def Lasso():
-    # Lasso
-    print("Lasso")
+    with mlflow.start_run():
+    
+        from scipy.stats import uniform
+        param_grid = {'alpha': uniform(0, 1)}
+        random_search = RandomizedSearchCV(model, param_distributions=param_grid, n_iter=100, cv=5, random_state=42)
+        random_search.fit(X_train, Y_train)
+        
+        # Logging best params
+        best_params = random_search.best_params_
+        for param_name, param_value in best_params.items():
+            mlflow.log_param(param_name, param_value)
+        
+        y_pred = random_search.predict(X_test)
+        mse = mean_squared_error(Y_test, y_pred)
+        
+        # Logging best score
+        mlflow.log_metric("mse", mse)
+        print("Mean Squared Error:", mse)
+        
+        # Logging the model to mlflow
+        mlflow.sklearn.log_model(random_search, "Lasso")
+
 
 def DecisionTreeRegressor():
     # Decision Tree Regressor
     print("Decision Tree Regressor")
     
 def ExtraTreesRegressor():
-    # Extra Trees Regressor
-    print("Extra Trees Regressor")
+    with mlflow.start_run():
+        
+        from scipy.stats import randint
+        param_dist = {
+            'n_estimators': randint(10, 200),
+            'max_features': ['auto', 'sqrt', 'log2'],
+            'max_depth': randint(1, 20),
+            'min_samples_split': randint(2, 20),
+            'min_samples_leaf': randint(1, 20),
+            'bootstrap': [True, False]
+        }
+        random_search = RandomizedSearchCV(estimator=et_model, param_distributions=param_dist, n_iter=100, cv=5, verbose=1, n_jobs=-1)
+        random_search.fit(X_train, Y_train)
+        
+        best_params = random_search.best_params_
+        best_model = random_search.best_estimator_
+        
+        # Logging best params
+        for param_name, param_value in best_params.items():
+            mlflow.log_param(param_name, param_value)
+            
+        y_pred = best_model.predict(X_test)
+        mse = mean_squared_error(Y_test, y_pred)
+        
+        # Logging best score
+        mlflow.log_metric("mse", mse)
+        
+        print("Mean Squared Error on Test Set:", mse)
+        
+        # Logging the model to mlflow
+        mlflow.sklearn.log_model(best_model, "ExtraTreesRegressor")
     
 def AdaBoostRegressor():
-    # Ada Boost Regressor
-    print("Ada Boost Regressor")
+    
+    with mlflow.start_run():
+        
+        from scipy.stats import randint
+        
+        param_dist = {
+        'n_estimators': randint(50, 200),
+        'learning_rate': [0.01, 0.1, 0.5, 1],
+        }
+        
+        random_search = RandomizedSearchCV(adaboost, param_distributions=param_dist, n_iter=10, cv=5, scoring='neg_mean_squared_error', random_state=42)
+        random_search.fit(X_train, Y_train)
+
+        best_params = random_search.best_params_
+        best_adaboost = random_search.best_estimator_
+
+        # Logging best params
+        for param_name, param_value in best_params.items():
+            mlflow.log_param(param_name, param_value)
+
+        y_pred = best_adaboost.predict(X_test)
+        mse = mean_squared_error(Y_test, y_pred)
+
+        # Logging best score
+        mlflow.log_metric("mse", mse)
+        print("Mean Squared Error on Test Set:", mse)
+
+        # Logging the model to mlflow
+        mlflow.sklearn.log_model(best_adaboost, "AdaBoostRegressor")
 
 models = {
         1: RandomForestRegressor,
@@ -149,13 +264,14 @@ models = {
 def train_model(model_id):
     
     if model_id in models:
+        
+        print(f"Training Model {models[model_id]}.")
+        
         # Executes the corresponding model function
         result = models[model_id]()
 
-        print(f"Training Model {models[model_id]}.")
+        
     else:
         print(f"Invalid model_id: {model_id}")
     
-train_model(1)
-    
-    
+train_model(4)
